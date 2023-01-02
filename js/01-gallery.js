42e6ff53b18daf5cb7b051adb8dac648a923d74a
OliwiaWawrzynek <oliwiawawrzynek@gmail.com>
2023-01-02 20:11:30 +0100

    goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItem = document.querySelector(".gallery");
+
+const handleClick = (e) => {
+
+const image = e.target.dataset.source;
+
+    if (e.target.dataset.source !== undefined ) {
+        const instance = basicLightbox.create(
+            ` <img src="${image}"> `
+        )
+        instance.show()
+
+        document.addEventListener("keydown", function escapeKey(event) {
+        if (event.key === "Escape") {
+        instance.close();
+        }
+        });
+    }
+    e.preventDefault();
+};
+
+galleryItem.addEventListener("click", handleClick, false);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
